cbffa883bd92272ffc271aaf7db3aced76a01ee0
sjolinjohan180 <sjolinjohan180@gmail.com>
2012-10-08 11:27:36 +0200

    changes to create, updating ids to index of array

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -1,24 +1,40 @@
-'user strict';
+'use strict';
 
 angularAppApp.controller('CreateCtrl', function($scope, $http) {
     $scope.questions = [];
 
-
     $scope.addQ = function(){
         $scope.questions.push({qId:$scope.questions.length+1, label:"", freeText: false, qType:"", answers: []});
-    }
+    };
 
     $scope.addA = function(q){
         q.answers.push({index:q.answers.length+1, value:""});
-    }
+    };
 
     $scope.removeQ = function(q){
-        $scope.questions.splice(q.index-1, 1);
-    }
+        var questions = $scope.questions;
+
+        questions = _.without(questions, q);
+        questions = $scope.cleanUpIds(questions);
+
+        $scope.questions = questions;
+    };
 
     $scope.removeA = function(q, a){
-        q.answers.splice(a.index-1, 1);
-    }
+        var answers = q.answers;
+
+        answers = _.without(answers, a);
+        answers = $scope.cleanUpIds(answers);
+
+        q.answers = answers;
+    };
+
+    $scope.cleanUpIds = function(array){
+        for(var i = 0; i < array.length; i++){
+            array[i].index = i + 1;
+        }
+        return array;
+    };
 
     $scope.qTypes = [];
 
